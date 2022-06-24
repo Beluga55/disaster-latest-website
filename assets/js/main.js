9b5fbbd6aeea2b4691e282bfe7a23e0d351b8cfa
beluga55 <beluga.github@gmail.com>
2022-06-24 13:55:36 +0800

    Done Neverlose Part And Breakpoints

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,3 +147,12 @@ const toggleItem = (item) => {
     item.classList.add("accordion-open");
   }
 };
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 350) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
